70e6750995235d5219d454f784fb81fb963b2136
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-10-08 12:00:36 +0200

    YOOOO

--- brumbrum/Assets/Scripts/HookesLaw.cs
+++ brumbrum/Assets/Scripts/HookesLaw.cs
@@ -5,35 +5,69 @@ public class HookesLaw : MonoBehaviour
     [Header("Properties")]
     public float springConstant = 50.0f;
     public float damping = 5.0f;
-    public float groundHeight = 0.0f;
-    
-    [Header("Keys")]
-    public KeyCode restartKey = KeyCode.Space;
+    public float distance = 0.0f;
+    public float rayLength = 10.0f;
+    public LayerMask groundLayer;
+    public Transform parent;
+    public int numberOfWheels = 4;
 
+    [Header("Offset")]
+    public Vector3 wheelOffset;
+
+    public float groundHeight = 0.0f;
+    public float distanceToGround;
     private Rigidbody rb;
-    private Vector3 startPosition;
+    private Rigidbody parentRb;
+    private Vector3 startLocalPosition;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        startPosition = this.transform.position;
+        parentRb = parent.GetComponent<Rigidbody>();
+        startLocalPosition = this.transform.localPosition;
     }
 
     void Update()
     {
-        if(Input.GetKey(restartKey))
+        CalculateDistanceToGround();
+
+        if (distanceToGround > 0)
         {
-            this.transform.position = startPosition;
+            groundHeight = transform.position.y - distanceToGround + distance;
         }
     }
 
     void FixedUpdate()
     {
-        float displacement = transform.position.y - groundHeight;
-
-        Vector3 springForce = new Vector3(0, -springConstant * displacement, 0);
-        Vector3 dampingForce = new Vector3(0, -damping * rb.velocity.y, 0);
+        Vector3 restPosition = parent.TransformPoint(startLocalPosition + wheelOffset);
+        Vector3 displacement = transform.position - new Vector3(restPosition.x, restPosition.y - distance, restPosition.z);
+        
+        Vector3 springForce = (-springConstant * displacement) / numberOfWheels; 
+        Vector3 dampingForce = (-damping * rb.velocity) / numberOfWheels;
 
         rb.AddForce(springForce + dampingForce);
+        parentRb.AddForceAtPosition(-springForce, transform.position);
+
+        Vector3 parentDampingForce = (-damping * parentRb.velocity) / numberOfWheels;
+        parentRb.AddForce(parentDampingForce);
+    }
+
+    void CalculateDistanceToGround()
+    {
+        Vector3 rayOrigin = transform.position;
+        RaycastHit hit;
+
+        Debug.DrawRay(rayOrigin, Vector3.down * rayLength, Color.red);
+
+        if (Physics.Raycast(rayOrigin, Vector3.down, out hit, rayLength, groundLayer))
+        {
+            distanceToGround = hit.distance;
+            Debug.Log("Distance to Ground: " + distanceToGround);
+        }
+        else
+        {
+            distanceToGround = -1;
+            Debug.Log("No ground detected.");
+        }
     }
 }
